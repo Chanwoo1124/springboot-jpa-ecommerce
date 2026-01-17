6e7c5f5afc5578e6ea5164f98a2f0f922dd3d69b
Chanwoo1124 <lgw22774@gmail.com>
2026-01-17 20:07:41 +0900

    template

--- src/main/java/jpabook/jpashop/controller/HomeController.java
+++ src/main/java/jpabook/jpashop/controller/HomeController.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,5 +14,8 @@ public class HomeController {
         log.info("home controller");
         return "home";
     }
+
+
+
 
 }
